b1136b4d25eadcd73de9368d8da290cfb7ec0743
Heorhii Kortunov <heorhiikortunov@gmail.com>
2025-07-25 00:36:41 +0200

    Added BarberController

--- src/main/java/com/example/demo/api/controller/BarberController.java
+++ src/main/java/com/example/demo/api/controller/BarberController.java
@@ -2,10 +2,14 @@ package com.example.demo.api.controller;
 
 import com.example.demo.api.dto.request.barber.CreateBarberDto;
 import com.example.demo.api.dto.request.barber.UpdateBarberDto;
+import com.example.demo.api.dto.request.user.UpdateUserDto;
 import com.example.demo.api.dto.response.barber.BarberResponseDto;
+import com.example.demo.api.dto.response.user.UserResponseDto;
+import com.example.demo.security.UserPrincipal;
 import com.example.demo.service.BarberService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,5 +20,14 @@ import java.util.List;
 public class BarberController {
 	private final BarberService barberService;
 
+	@GetMapping("/me")
+	public ResponseEntity<BarberResponseDto> findMyProfile(@AuthenticationPrincipal UserPrincipal principal){
+		return ResponseEntity.ok(barberService.findById(barberService.findByUserId(principal.getUserId()).id()));
+	}
+
+	@PutMapping("/me")
+	public ResponseEntity<BarberResponseDto> updateMyProfile(@AuthenticationPrincipal UserPrincipal principal, UpdateBarberDto dto){
+		return ResponseEntity.ok(barberService.updateBarber(barberService.findByUserId(principal.getUserId()).id(), dto));
+	}
 
 }
