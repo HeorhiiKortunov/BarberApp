268c0451f5fc18139a0b8ebb66b6bc2e23f5c34d
Heorhii Kortunov <heorhiikortunov@gmail.com>
2025-07-29 17:58:45 +0200

    Added AuthControllerTest

---
+++ src/test/java/com/example/demo/api/controller/AuthControllerTest.java
@@ -0,0 +1,92 @@
+package com.example.demo.api.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AuthControllerTest {
+	@Autowired
+	private MockMvc api;
+
+	//register tests
+	@Test
+	void givenValidUser_whenRegister_thenCanLoginSuccessfully() throws Exception {
+		String registerJson = """
+        {
+          "username": "John",
+          "password": "secret",
+          "email": "john@example.com",
+          "phone": "1234567890"
+        }
+    """;
+
+		// register user
+		api.perform(post("/api/auth/register")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(registerJson))
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.email").value("john@example.com"));
+
+		String loginJson = """
+        {
+          "username": "John",
+          "password": "secret"
+        }
+    """;
+
+		// login with same user
+		api.perform(post("/api/auth/login")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(loginJson))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.jwt").isNotEmpty());
+	}
+
+
+	@Test
+	void givenBadUser_whenRegister_shouldReturnCreated() throws Exception {
+		String json = """
+            {
+              "username": "John",
+            }
+            """;
+
+		api.perform(post("/api/auth/register")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(json))
+				.andExpect(status().isBadRequest());
+	}
+
+	//login tests
+	@Test
+	void givenValidCredentials_whenLogin_shouldReturnLoginResponse(){
+
+	}
+
+	@Test
+	void givenInvalidCredentials_whenLogin_thenUnauthorized() throws Exception {
+		String json = """
+        {
+          "username": "wrong",
+          "password": "wrong"
+        }
+        """;
+
+		api.perform(post("/api/auth/login")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(json))
+				.andExpect(status().isUnauthorized());
+	}
+
+}
